0957ee7225bbfbb4c965aef06afe2394d3af3632
Vinitkatariya <60475638+Vinitkatariya@users.noreply.github.com>
2022-05-24 20:21:22 +0530

    Add files via upload

---
+++ lib/screens/home/Equipments/selleq/searchselleq.dart
@@ -0,0 +1,17 @@
+//import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+class SearchSelleq extends StatefulWidget {
+  @override
+  _SearchSelleqState createState() => _SearchSelleqState();
+}
+
+class _SearchSelleqState extends State<SearchSelleq> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
